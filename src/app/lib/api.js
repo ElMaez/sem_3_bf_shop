4e5fbba3949532e23b3b8cc688cb8e186caa9e22
Mai El-Zein <mai_ez@outlook.dk>
2025-04-23 11:12:45 +0200

    env file was not in the root, so now it works after it was moved

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,8 +1,6 @@
 // Everything Api so we can just call info from one place.
 export async function getProducts() {
-  const data = await fetch("https://dummyjson.com/products");
-  //   "https://dummyjson.com/products"  --- Denne virker
-  // process.env.PRODUCTS_URL -- virker IKKE
+  const data = await fetch(process.env.PRODUCTS_URL);
   const products = await data.json();
   console.log("api.js :", products);
   return products.products;
