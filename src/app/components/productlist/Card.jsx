417283da4ef93144b8beb292eb9364fff9ac5c98
Mai El-Zein <mai_ez@outlook.dk>
2025-04-24 11:15:50 +0200

    IT'S AAAALIVEEE and works

--- src/app/components/productlist/Card.jsx
+++ src/app/components/productlist/Card.jsx
@@ -1,8 +1,9 @@
+"use client";
 import Link from "next/link";
 
 const Card = (item) => {
   return (
-    <Link href={`/${item.id}`}>
+    <Link href={`/productView/${item.id}`}>
       <h1>{item.title}</h1>
     </Link>
   );
--- src/app/productView/[id]/page.jsx
+++ src/app/productView/[id]/page.jsx
@@ -3,12 +3,15 @@ import Card from "@/app/components/productlist/Card";
 import Gallery from "@/app/components/product/Gallery";
 import Reviews from "@/app/components/product/Reviews";
 import Button from "@/app/components/other/Button";
+import { getProductId } from "@/app/lib/api";
 
-export default async function Product() {
-  const product = await getProductId();
+export default async function Product({ params }) {
+  const { id } = await params;
+  const item = await getProductId(id);
+  console.log("productView page.jsx :", "product: ", item, "id: ", id);
   return (
     <main>
-      <h1>{product.id}</h1>
+      <h1>{item.id} Her er et product</h1>
       <Button />
       {/* Der skal vises product af id fået fra productlist */}
       <Gallery />
