8ce7dad3ac3b7d5948a21ada592c27252036ae9f
eljeys <eljeys1@gmail.com>
2025-04-24 12:06:44 +0200

    Basket serverside working

--- src/app/components/other/BasketServerSide.jsx
+++ src/app/components/other/BasketServerSide.jsx
@@ -1,36 +1,11 @@
 import { getProducts } from "@/app/lib/api";
 import Basket from "./Basket";
+import Product from "@/app/productView/[id]/page";
 
-export async function getServerSideProps() {
-  try {
-    const fetchData = getProducts();
-    console.log("ER i Basket server", fetchData);
-    return {
-      props: {
-        products: data.products || [],
-      },
-    };
-  } catch (error) {
-    console.error("Fejl ved hentning af produkter:", error);
-    return {
-      props: {
-        products: [],
-      },
-    };
-  }
-}
-
-const ProductsPage = ({ products }) => {
-  return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {products.map((products) => (
-        <Basket key={products.id} products={products} />
-      ))}
-    </div>
-  );
+const BasketServerSide = async () => {
+  const products = await getProducts();
+  console.log("ER i Basket server", products);
+  return <Basket data={products} />;
 };
 
-export default ProductsPage;
-
-// const products = await getProducts();
-// return <Basket />;
+export default BasketServerSide;
