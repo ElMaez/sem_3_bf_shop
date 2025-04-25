00ea8c5b876c4a82e8c922b536c3e7729e8fb9b5
ElMaez <mai_ez@outlook.dk>
2025-04-25 10:11:35 +0200

    to get started

--- src/app/components/productlist/SearchBar.jsx
+++ src/app/components/productlist/SearchBar.jsx
@@ -1,5 +1,13 @@
+import Button from "../other/Button";
+
 const SearchBar = () => {
-  return {};
+ const document.quarySelector("#")
+  return (
+    <forms>
+      <input type="text" placeholder="search here"></input>
+      <input type="button" value="Search"></input>
+    </forms>
+  );
 };
 
 export default SearchBar;
