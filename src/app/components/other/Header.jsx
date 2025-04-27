2f9eac5bd2f301cffb7bb4c118125d9b715f7b19
eljeys <eljeys1@gmail.com>
2025-04-27 17:15:27 +0200

    Header_v1

--- src/app/components/other/Header.jsx
+++ src/app/components/other/Header.jsx
@@ -1,17 +1,50 @@
+"use client";
 import Link from "next/link";
-
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Logo from "@/app/assets/img/logo.png";
 const Header = () => {
+  const pathname = usePathname();
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/productlist">Product</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className="flex items-center p-4 justify-between">
+      <div>
+        <Link href="/">
+          <Image src={Logo} width={250} height={80} alt="logo" />
+        </Link>
+      </div>
+      <nav className=" flex">
+        <ul className="flex flex-row gap-8">
+          <li>
+            <Link
+              href="/"
+              className={` ${
+                pathname === "/"
+                  ? "bg-white py-4 px-2 text-black font-bold"
+                  : "text-white  hover:underline "
+              }`}
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/productlist"
+              className={` ${
+                pathname === "/productlist"
+                  ? "bg-white py-4 px-2 text-black font-bold"
+                  : "text-white hover:underline "
+              }`}
+            >
+              Product
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="">
+        <MdOutlineShoppingBag size={36} />
+      </div>
+    </header>
   );
 };
 
