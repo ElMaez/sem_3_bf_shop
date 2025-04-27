4d6a2f11d96dd3e5a0c2b2b10b9dfbfabd4296aa
eljeys <eljeys1@gmail.com>
2025-04-27 15:34:41 +0200

    removed import

--- src/app/components/other/Basket.jsx
+++ src/app/components/other/Basket.jsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { getProducts } from "@/app/lib/api";
 import Image from "next/image";
 import { FaPlus } from "react-icons/fa6";
 import { FaMinus } from "react-icons/fa6";
