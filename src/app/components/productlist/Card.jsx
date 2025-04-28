bbebd063bc5ec927a4a2eb865ebc49422c473f7d
Amanda <amhu0002@stud.kea.dk>
2025-04-28 14:08:27 +0200

    design af cards og filter

--- src/app/components/productlist/Card.jsx
+++ src/app/components/productlist/Card.jsx
@@ -1,75 +1,88 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import { FaRegStar } from "react-icons/fa6";
+import { FaRegStar, FaStar } from "react-icons/fa";
 import { MdOutlineShoppingBag } from "react-icons/md";
-import Product from "@/app/productView/[id]/page";
 import useCartStore from "@/app/stores/increaseAmount";
+
 const Card = (item) => {
   console.log("Card wallah:", item.id);
-  console.log("images fuckhoved: ", item.images[0]);
-  const discountPercentage = `${item.discountPercentage / 100}`;
-  const newPrice = Math.floor(`${item.price * (1 - discountPercentage)}`);
-  const price = Math.floor(`${item.price}`);
+  console.log("images fuckhoved: ", item.images?.[0]);
+  const discountPercentageDecimal = item.discountPercentage / 100;
+  const newPrice = Math.floor(item.price * (1 - discountPercentageDecimal));
+  const oldPrice = Math.floor(item.price);
 
-  // henter addItem() fra Zustand store
   const addItemToBasket = useCartStore((state) => state.addItem);
 
-  //eventhandler for for kurve-ikonet
   const handleAddToBasket = (event) => {
     event.stopPropagation();
-    addItemToBasket(item); //HUSK at ADD to Zustand Store.
+    addItemToBasket(item);
   };
 
-  console.log(
-    `"DISCOUNT I DECIMAL: ",
-    ${newPrice} , "PROCENTEN ER", ${item.discountPercentage} "DEN GAME PRIS ER: "${price}`
-  );
+  const renderStars = (rating) => {
+    const stars = [];
+    const fullStars = Math.floor(rating);
+
+    for (let i = 1; i <= 5; i++) {
+      if (i <= fullStars) {
+        stars.push(<FaStar key={`full-${i}`} className="text-[#A49C96]" />);
+      } else {
+        stars.push(<FaRegStar key={`empty-${i}`} className="text-[#A49C96]" />);
+      }
+    }
+    return <span className="flex items-center">{stars}</span>;
+  };
 
   return (
-    <section className="border-2 relative grid">
+    <section className="border-2 border-[#F9F6F2] relative grid shadow-md">
+      {" "}
       <Link
         href={`/product/${item.id}`}
         className="after:content-[' ']  after:inset-0 hover:cursor-pointer block "
       >
-        <figure className="w-3xs">
-          <Image
-            src={item.images[0]}
-            width={240}
-            height={140}
-            alt={item.title}
-          />
+        <figure
+          className="w-3xs relative overflow-hidden flex has-[img]:justify-self-center "
+          style={{ aspectRatio: "1 / 1" }}
+        >
+          {item.images?.[0] && (
+            <Image
+              src={item.images[0]}
+              alt={item.title}
+              layout="fill"
+              objectFit="contain"
+            />
+          )}
         </figure>
         <section className="p-4">
-          <span className="absolute bg-amber-300 font-bold z-10 top-0.5 left-0.5 text-black p-[0.2rem]">
+          <span className="absolute bg-amber-300 font-medium z-10 top-0.5 left-0.5 text-black p-[0.2rem] shadow-sm">
+            {" "}
             {item.discountPercentage}%
           </span>
-          <span className="absolute bg-red-600 font-bold bottom-28 right-2 p-[0.2rem] ">
-            on stock: {item.stock}
-          </span>
-          <h1 className="text-[1rem]">{item.title}</h1>
-          <div className="flex flex-col">
-            <span className="mt-2">{newPrice}$</span>
-            <span className="font-bold text-gray-400 flex gap-1">
-              <p>Old price:</p> <p className="line-through">{price}$</p>
+          {item.stock <= 5 && item.stock > 0 && (
+            <span className="absolute bg-red-600 bottom-28 right-2 p-[0.2rem] text-white">
+              Low stock
             </span>
+          )}
+          {item.stock === 0 && (
+            <span className="absolute bg-red-600 bottom-28 right-2 p-[0.2rem] text-white">
+              Out of stock
+            </span>
+          )}
+          <h1 className="text-[1rem]">{item.title}</h1>
+          <div className="flex items-center gap-2">
+            <span>{newPrice}$</span>
+            {item.discountPercentage > 0 && (
+              <span className=" text-[#8F8A85] line-through">{oldPrice}$</span>
+            )}
           </div>
         </section>
       </Link>
       <div className="flex gap-2 items-center justify-between p-4">
         <div className="flex gap-2 items-center">
-          <FaRegStar />
-          <FaRegStar />
-          <FaRegStar />
-          <FaRegStar />
-          <FaRegStar />
-          <span className="text-[0.8rem] text-gray-300">{item.rating}</span>
+          {renderStars(item.rating)}
         </div>
-        <button
-          onClick={handleAddToBasket}
-          className="border-2 hover:text-red-600"
-        >
-          <MdOutlineShoppingBag size={24} className="hover:text-red-600" />
+        <button onClick={handleAddToBasket} className=" hover:text-[#8F8A85]">
+          <MdOutlineShoppingBag size={24} className="hover:text-[#8F8A85]" />
         </button>
       </div>
     </section>
