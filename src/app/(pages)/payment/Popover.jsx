0bba3d4fae7055efc15dee83bb3ebc6002dfb82d
ElMaez <mai_ez@outlook.dk>
2025-04-30 14:13:05 +0200

    mangler at få anchor til at virke.

---
+++ src/app/(pages)/payment/Popover.jsx
@@ -0,0 +1,59 @@
+// @flow strict
+"use client";
+import { useEffect, useRef, useState } from "react";
+
+function Popover({ children, content, trigger = "click" }) {
+  const [show, setShow] = useState(false);
+  const wrapperRef = useRef(null);
+
+  const handleMouseOver = () => {
+    if (trigger === "hover") {
+      setShow(true);
+    }
+  };
+
+  const handleMouseLeft = () => {
+    if (trigger === "hover") {
+      setShow(false);
+    }
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (wrapperRef.current && !wrapperRef.current.contains(event.target)) {
+        setShow(false);
+      }
+    }
+
+    if (show) {
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  }, [show, wrapperRef]);
+
+  return (
+    <div
+      style={{ positionAnchor: "--paymentpopup" }}
+      ref={wrapperRef}
+      onMouseEnter={handleMouseOver}
+      onMouseLeave={handleMouseLeft}
+      className="w-fit h-fit relative flex"
+    >
+      <div onClick={() => setShow(!show)}>{children}</div>
+      <div
+        hidden={!show}
+        className=" absolute top-[0%] left-[0%] z-50 transition-all bg-amber-500 "
+      >
+        <div className="rounded-xs w-[500px] h-52 bg-white shadow-[10px_30px_150px_rgba(46,38,92,0.25)] p-[1rem] grid">
+          {content}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Popover;
